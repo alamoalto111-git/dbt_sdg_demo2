cc8fada5e9b4209a8e36ace350019057f01cb158
Alejandra Goñi <alamoaltosnow@gmail.com>
2025-10-22 17:01:35 +0000

    subiendo_22-10

--- models/raw_vault/hub_order.sql
+++ models/raw_vault/hub_order.sql
@@ -1,15 +1,16 @@
-{{ config(materialized='incremental', unique_key='hub_order_sk' ) }}
+{{ config(materialized='incremental', unique_key='hub_order_hk' ) }}
 
 with src as (  
   select * from {{ ref('stg_orders') }}
 ),
 hub as (  
   select    
-     abs(hash(o_orderkey)) as hub_order_sk,
+     {{ dv_hash(['o_orderkey']) }} as hub_order_hk,
+     --abs(hash(o_orderkey)) as hub_order_sk,
      --sha2(concat(o_orderkey),256) as hub_order_sk,
      --md5(o_orderkey) as hub_order_skmd,     
      o_orderkey          as business_key,    
-     'ORDER'          as entity_name,
+     'ORDER'             as entity_name,
      '{{ ref('stg_orders') }}'          as record_source,
      '{{ ref('stg_orders').name }}'     as record_source_md,
      '{{ ref('stg_orders').schema }}'   as record_source_sc,
@@ -20,7 +21,7 @@ hub as (
 
 select * from hub
 {% if is_incremental() %}
-where hub_order_sk not in (select hub_order_sk from {{ this }})
+where hub_order_hk not in (select hub_order_hk from {{ this }})
 {% endif %}
 
 
--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -3,16 +3,19 @@
 with raw as (
     select * from {{ source('tpch_sf1', 'customer') }}
     ---SNOWFLAKE_SAMPLE_DATA_AG.TPCH_SF1.CUSTOMER
-    where c_custkey BETWEEN 99998 AND 100100
+    --where c_custkey BETWEEN 99998 AND 100010 --100100
+     where c_custkey in 
+         (select o_custkey from SNOWFLAKE_SAMPLE_DATA_AG.TPCH_SF1.orders
+         where o_orderkey between 4200001 and 4200034) 
+
 )
 select
- 
   c_custkey,--::varchar as c_custkey,  
-  c_name,             ---  as c_name,  
-  c_address, --as c_address,  
-  c_nationkey::varchar as c_nationkey,  
-  c_phone as c_phone,  
-  c_acctbal::numeric as c_acctbal,  
+  c_name,             
+  c_address,
+  c_nationkey, --as c_nationkey,  
+  c_phone,  
+  c_acctbal,--::numeric as c_acctbal,  
   c_mktsegment,  
   c_comment
 from raw
